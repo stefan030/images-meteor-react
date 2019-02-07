e01ca0fa6e32ecbd7d73932add22e3a67befb517
stelle030 <stefanbogdanovic030@gmail.com>
2019-02-07 11:40:11 +0100

    Twitter bootstrap implementation and styling

--- client/components/image_detail.js
+++ client/components/image_detail.js
@@ -6,10 +6,16 @@ const ImageDetail = (props) => {
     // props.image.link
 
   return(
-      <div>
-        <div>{props.image.title}</div>
-        <img src={props.image.link}></img>
-      </div>
+      <li className='media list-group-item'>
+        <div className='media-left'>
+            <img src={props.image.link}></img>
+        </div>
+        <div className='media-body'>
+            <h4 className='media-heading'>
+                {props.image.title}
+            </h4>
+        </div>
+      </li>
   )
 };
 
--- client/components/image_list.js
+++ client/components/image_list.js
@@ -22,7 +22,7 @@ const ImageList = () => {
     });
 
     return(
-        <ul>
+        <ul className='media-list list-group'>
             { RenderedImages }
         </ul>
     )
